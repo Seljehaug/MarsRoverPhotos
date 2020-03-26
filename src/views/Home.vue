c509d1c2ba0511ee524e5124be83c11955d7c470
Christoffer Seljehaug <cs_seljehaug@hotmail.com>
2020-03-26 20:36:34 +0100

    removed eslint, set up basic project structure, connected to the api to get the rover manifests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Home</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+  import Vue from 'vue';
+  import { NASA_API_KEY } from '../../configuration/API_KEY';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  interface IHome {
+    curiosityManifest: any;
+    opportunityManifest: any;
+    spiritManifest: any;
+
+    getManifest(rover: Rovers): void;
   }
-}
+
+  export enum Rovers {
+    Curiosity = 0,
+    Opportunity = 1,
+    Spirit = 2
+  }
+
+  export default Vue.extend({
+    name: 'Home',
+    props: {},
+    data() {
+      return {
+        curiosityManifest: null,
+        opportunityManifest: null,
+        spiritManifest: null
+      }
+    },
+    mounted() {
+      const self = this as IHome;
+
+      self.getManifest(Rovers.Curiosity);
+      self.getManifest(Rovers.Opportunity);
+      self.getManifest(Rovers.Spirit);
+    },
+    methods: {
+      getManifest(rover: Rovers) {
+        const self = this as IHome;
+        const roverName = Rovers[rover].toLowerCase();
+
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?api_key=${NASA_API_KEY}`)
+          .then(response => {
+            return response.json()
+              .then(data => {
+                switch (rover) {
+                  case Rovers.Curiosity: {
+                    self.curiosityManifest = data.photo_manifest;
+                    break;
+                  }
+                  case Rovers.Opportunity: {
+                    self.opportunityManifest = data.photo_manifest;
+                    break;
+                  }
+                  case Rovers.Spirit: {
+                    self.spiritManifest = data.photo_manifest;
+                    break;
+                  }
+                }
+              });
+          });
+      }
+    }
+  });
 </script>
+
+<style scoped lang="scss">
+
+</style>
